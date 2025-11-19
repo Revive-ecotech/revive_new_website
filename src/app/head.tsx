f2e800d13518ca08e3f40d3cd8bb9a782e0d48b5
Revive-ecotech <reviveechotech@gmail.com>
2025-11-19 08:20:50 +0530

    Create head.tsx

---
+++ src/app/head.tsx
@@ -0,0 +1,12 @@
+export default function Head() {
+  return (
+    <>
+      <meta
+        name="google-site-verification"
+        content="ADlb8HGH4MnniAFAryH0KHNuH_7EMyNZtTA_hGJAT18"
+      />
+      <title>Revive EcoTech</title>
+      <meta name="description" content="Earn by recycling waste" />
+    </>
+  );
+}
