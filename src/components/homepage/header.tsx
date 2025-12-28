94d12309ffcfea12eeeca68eb8b0b73b91e941c9
Revive EcoTech <reviveechotech@gmail.com>
2025-12-28 19:49:28 +0530

    Update header.tsx

--- src/components/homepage/header.tsx
+++ src/components/homepage/header.tsx
@@ -10,7 +10,7 @@ const Header = () => {
             <div className='flex flex-col md:flex-row gap-7 items-center mt-14' >
                 <AnimatedTooltipPreview />
                 <div className='text-xs text-neutral-500'>
-                    Trusted by thousands of healthy people
+                    Trusted by thousands of people
                 </div>
             </div>
             <div className="w-full sm:w-5/6 md:w-2/3 lg:w-1/2 mx-auto px-5 sm:px-2 ">
